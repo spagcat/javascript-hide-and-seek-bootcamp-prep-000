37723008ab71c03778d849dbfa03f72d993e8f6f
Shel Orock <sdo243@nyu.edu>
2018-04-18 06:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function getFirstSelector(selector) {
   var firstElement = document.querySelector(selector);
   return firstElement;
 }
+
+function nestedTarget() {
+  var nestedTarget = document.getElementById(#nested)
+}
