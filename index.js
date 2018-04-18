e2a30e344ab49f271e7fbd8ff6b32f2eda1ac6b5
Shel Orock <sdo243@nyu.edu>
2018-04-18 06:56:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var target = document.getElementByClassName('ranked-list')
+  var target = document.querySelectorAll('ul.ranked-list')
 }
